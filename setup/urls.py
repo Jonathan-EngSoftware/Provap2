bfb35b3894a5f63e35cede7c37f1b5c45a862c08
Jonathan <jonathan.eletromecanico@hotmail.com>
2024-11-19 19:49:04 -0300

    prova p2

---
+++ setup/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from todos.views import TodoListView, TodoCreateView
+
+urlpatterns = [
+    path('', TodoListView.as_view(), name='todo_list'),
+    path('create/', TodoCreateView.as_view(), name='todo_create')
+]
